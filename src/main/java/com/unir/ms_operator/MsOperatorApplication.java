da06067451f6ee7674caf12996be67946f024387
Nelson Sambula <nelson.sambula@movitext.com>
2024-08-17 13:10:02 -0600

    chore: Add initial project files and configuration

---
+++ src/main/java/com/unir/ms_operator/MsOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.unir.ms_operator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/ms_operator/MsOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.ms_operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
